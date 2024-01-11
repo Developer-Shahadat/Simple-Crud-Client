5163f00dbb3381d179c1fd2dc95b70206422c649
Developer-Shahadat <shahadatbci95@gmail.com>
2024-01-11 22:06:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { NavLink, Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const formUser = { name, email };
+    console.log(formUser);
+
+    // Server Site a data pathano er jonno
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("User Add SuccessFull");
+          form.reset();
+        }
+      });
+  };
+
+  return (
+    <>
+      <NavLink to='/users'>User List</NavLink>
+      <Outlet></Outlet>
+
+      <h1>Simple Crud Client</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { NavLink, useLoaderData } from "react-router-dom";
+
+const Users = () => {
+  const userData = useLoaderData();
+  const [users, setUsers] = useState(userData);
+  const handleDeleteUSer = (_id) => {
+    console.log("delete user", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("User deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      });
+  };
+  return (
+    <div>
+      <h1>Number Of User : {users.length}</h1>
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} : {user.email}{" "}
+            <button onClick={() => handleDeleteUSer(user._id)}>Delete</button>
+          </p>
+        ))}
+      </div>
+      <NavLink to="/">Go Back</NavLink>
+    </div>
+  );
+};
+
+export default Users;
